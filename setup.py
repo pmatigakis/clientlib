a448f5ecb18c61caee0aea7a0e17b570ecb81d5d
Matigakis Panagiotis <pmatigakis@gmail.com>
2020-09-26 13:07:27 +0300

    Added additional classifiers to the setup.py file

--- setup.py
+++ setup.py
@@ -42,8 +42,14 @@ setup(
     include_package_data=True,
     zip_safe=True,
     classifiers=(
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries"
     )
 )
